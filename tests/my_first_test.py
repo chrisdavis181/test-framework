e93839031eafba750082ad015a29fc9fd77e759a
Chris Davis <christopher.davis@asics.com>
2024-01-27 01:38:23 -0500

    initial commit

---
+++ tests/my_first_test.py
@@ -0,0 +1,27 @@
+from seleniumbase import BaseCase
+BaseCase.main(__name__, __file__)
+
+
+class MyTestClass(BaseCase):
+    def test_swag_labs(self):
+        self.open("https://www.saucedemo.com")
+        self.type("#user-name", "standard_user")
+        self.type("#password", "secret_sauce\n")
+        self.assert_element("div.inventory_list")
+        self.assert_text("Products", "span.title")
+        self.click('button[name*="backpack"]')
+        self.click("#shopping_cart_container a")
+        self.assert_text("Your Cart", "span.title")
+        self.assert_text("Backpack", "div.cart_item")
+        self.click("button#checkout")
+        self.type("#first-name", "SeleniumBase")
+        self.type("#last-name", "Automation")
+        self.type("#postal-code", "77123")
+        self.click("input#continue")
+        self.assert_text("Checkout: Overview")
+        self.assert_text("Backpack", "div.cart_item")
+        self.click("button#finish")
+        self.assert_exact_text("Thank you for your order!", "h2")
+        self.assert_element('img[alt="Pony Express"]')
+        self.js_click("a#logout_sidebar_link")
+        self.assert_element("div#login_button_container")
